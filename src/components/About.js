619f37055233c050f61b11d0a61f39d834dd304e
James Lee <uptownhr@gmail.com>
2016-05-27 13:29:04 -0700

    added react-router

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class About extends React.Component{
+  constructor(){
+    super()
+  }
+
+  render(){
+    const sections = this.sections
+
+    return (
+      <div>
+        About page
+      </div>
+    )
+  }
+}
+
+export default About
